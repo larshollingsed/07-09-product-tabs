8177a7652de29b1aa952a45f19caa77740423d63
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-09 22:09:15 -0500

    Tabs for product1

---
+++ public/global.js
@@ -0,0 +1,39 @@
+document.getElementById("product1general_info_tab").onclick = show_product1_general_info;
+
+function show_product1_general_info() {
+  document.getElementById("product1general_info").className = "content_template";
+  document.getElementById("product1technical_specs").className = "content_template hidden";
+  document.getElementById("product1where_to_buy").className = "content_template hidden";
+}
+
+document.getElementById("product1technical_specs_tab").onclick = show_product1_technical_specs;
+
+function show_product1_technical_specs() {
+  document.getElementById("product1general_info").className = "content_template hidden";
+  document.getElementById("product1technical_specs").className = "content_template";
+  document.getElementById("product1where_to_buy").className = "content_template hidden";
+}
+
+document.getElementById("product1where_to_buy_tab").onclick = show_product1_where_to_buy;
+
+function show_product1_where_to_buy() {
+  document.getElementById("product1general_info").className = "content_template hidden";
+  document.getElementById("product1technical_specs").className = "content_template hidden";
+  document.getElementById("product1where_to_buy").className = "content_template";
+}
+
+// var number_of_products = 5;
+// var gen_info;
+// var tech_specs;
+// var where;
+// for (counter = 1; counter < number_of_products; counter++) {
+//   gen_info = "product" + counter + "general_info";
+//
+//   document.getElementById("product1general_info_tab").onclick = show_product1_general_info;
+//
+//   function show_product1_general_info() {
+//     document.getElementById(gen_info).className = "content_template";
+//     document.getElementById("product1technical_specs").className = "content_template hidden";
+//     document.getElementById("product1where_to_buy").className = "content_template hidden"
+//   }
+// }
