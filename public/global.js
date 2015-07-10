f2ff0be86aeebfaadde7f96aef8f0930c935561e
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-09 23:43:56 -0500

    tried to do a loop for the .onclick and its functions...fail :(

--- public/global.js
+++ public/global.js
@@ -28,12 +28,15 @@ function show_product1_where_to_buy() {
 // var where;
 // for (counter = 1; counter < number_of_products; counter++) {
 //   gen_info = "product" + counter + "general_info";
+//   tech_specs = "product" + counter + "technical_specs";
+//   where = "product" + counter + "where_to_buy";
 //
 //   document.getElementById("product1general_info_tab").onclick = show_product1_general_info;
+//   var function_name = "show_product" + counter + "_general_info"
 //
-//   function show_product1_general_info() {
+//   function function_name() {
 //     document.getElementById(gen_info).className = "content_template";
-//     document.getElementById("product1technical_specs").className = "content_template hidden";
-//     document.getElementById("product1where_to_buy").className = "content_template hidden"
+//     document.getElementById(tech_specs).className = "content_template hidden";
+//     document.getElementById(where).className = "content_template hidden"
 //   }
 // }
