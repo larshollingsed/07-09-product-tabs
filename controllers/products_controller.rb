694c8c8a6603bd5072552550e0e58a99436ed7e8
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-09 19:55:57 -0500

    Basic 'see all products' view

--- controllers/products_controller.rb
+++ controllers/products_controller.rb
@@ -6,3 +6,7 @@ get "/add_product_confirm" do
   @new_product = Product.add(params["product"])
   erb :"home"
 end
+
+get "/see_all_products" do
+  erb :"products/see_all_products"
+end
--- database_setup.rb
+++ database_setup.rb
@@ -1,3 +1,5 @@
 DB = SQLite3::Database.new("products.db")
 
 DB.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, product_name TEXT, general_info TEXT, technical_specs TEXT, where_to_buy TEXT);")
+
+DB.results_as_hash = true
